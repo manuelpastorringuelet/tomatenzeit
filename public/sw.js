a0ace8ae4131d202235ee222a298a690596a9789
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2024-09-17 22:23:35 +0200

    feat(pwa): enhance Progressive Web App support
    - Add new app icon sizes (72x72, 96x96, 128x128, 144x144, 152x152, 384x384)
    - Update manifest.json with new icon entries
    - Increase service worker cache version to v2
    - Add new icon files to service worker cache list

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,15 @@
-const CACHE_NAME = 'pomodoro-wellness-v1';
+const CACHE_NAME = 'pomodoro-wellness-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/icon-72x72.png',
+  '/icon-96x96.png',
+  '/icon-128x128.png',
+  '/icon-144x144.png',
+  '/icon-152x152.png',
   '/icon-192x192.png',
+  '/icon-384x384.png',
   '/icon-512x512.png',
   '/notification.mp3',
   '/pomodoro-wellness-image.png',
